1d57166ea2584c1f003c7acbeef3df7afbafb929
Alister Machado <alister.reis@gmail.com>
2013-05-13 22:25:09 -0300

    Tinha dado umas merdas... re-commitei o original que fizemos hoje de manhã.

--- formais.py
+++ formais.py
@@ -1,6 +1,6 @@
 #-*- coding: utf-8 -*-
 import sys
-from formaisFuncoes import *
+
 
 
 regras = { }
@@ -14,7 +14,37 @@ inicial = None
 
 gramatica = (variaveis,terminais,regras,inicial)
 
+def achaTerminais(linhaTerminais):
+    global terminais
+    for ter in linhaTerminais.strip('{ ,}\n').split(', '):
+        terminais.append(ter)
 
+def achaVariaveis(linhaVariaveis):
+    global variaveis
+    for var in linhaVariaveis.strip('{ ,}\n').split(', '):
+        variaveis.append(var)
+
+def formataArquivo(arq):
+    global arquivo
+    for line in arq:
+        arquivo.append(line.partition('#')[0].strip())
+
+def processaRegras(linhaInicial):
+    global arquivo
+    for i in range(linhaInicial, len(arquivo)):
+        linha = arquivo[i].strip('{ ,}\n').replace(',', '')
+        regra, dummy, prob = linha.partition('}')
+        esquerda, dummy,  direita = regra.partition(' > ')
+        try:
+            if (regras[esquerda] == None): regras[esquerda] = []
+        except KeyError:
+            regras[esquerda] = []
+        regras[esquerda].append([direita.strip(), float(prob.strip('; '))])
+        
+def found(word, listOfLists):
+    for l in listOfLists:
+        if word in l: return True
+    return False
 formataArquivo(sys.stdin)
 
 rang = range(len(arquivo))
